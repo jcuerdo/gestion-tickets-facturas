37e16a3794736eb668492c275dbbdd875e127f2e
Jorge Cuerdo <jcuerdo@magento.com>
2017-01-02 23:31:09 +0100

    Email headers

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -125,13 +125,15 @@ namespace Controller
             else if( 'email' == $version )
             {
                 $email = $app['request']->get( 'email' );
+                $shopName = $shop['name'];
+
                 $headers = "From: <info@peluqueria-carmen.es>\r\n";
                 $headers .= "MIME-Version: 1.0\r\n";
                 $headers .= "Content-type:text/html;charset=UTF-8\r\n";
 
                 $emailSent = mail(
                     $email,
-                    sprintf('Tickets desde %s hasta %s de Peluqueria Carmen', $start_date, $end_date),
+                    sprintf("Tickets desde %s hasta %s de $shopName", $start_date, $end_date),
                     $this->getPrintVersion($app, $report, $start_date, $end_date, $shop),
                     $headers
             );
